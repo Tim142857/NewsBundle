2d74734f6ddc5d865f14110d801c8e6aec80d2f3
Tim142857 <timothyleroch@gmail.com>
2016-09-24 19:17:21 +0200

    work in progress

--- NewsBundle.php
+++ NewsBundle.php
@@ -3,7 +3,16 @@
 namespace Tleroch\NewsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tleroch\NewsBundle\DependencyInjection\NewsExtension;
 
-class NewsBundle extends Bundle
-{
+class NewsBundle extends Bundle {
+
+    public function getContainerExtension() {
+        if (null === $this->extension) {
+            $this->extension = new NewsExtension();
+        }
+
+        return $this->extension;
+    }
+
 }
